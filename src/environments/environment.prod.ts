b36a5a3ec5f1e2a1bd24589eee0f7c90d25b5d1c
skori <skori@dminc.com>
2021-05-31 10:57:55 +0530

    working angular material 7 firebase todo

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDPN2vYTK1KL4PGO8j0w4tev0yf8ILWl_0",
+    authDomain: "ssvtodos.firebaseapp.com",
+    databaseURL: "https://ssvtodos-default-rtdb.firebaseio.com",
+    projectId: "ssvtodos",
+    storageBucket: "ssvtodos.appspot.com",
+    messagingSenderId: "20219089328",
+    appId: "1:20219089328:web:488e8bb0227222b2c54675",
+    measurementId: "G-81GSEP6L1D"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDPN2vYTK1KL4PGO8j0w4tev0yf8ILWl_0",
+    authDomain: "ssvtodos.firebaseapp.com",
+    databaseURL: "https://ssvtodos-default-rtdb.firebaseio.com",
+    projectId: "ssvtodos",
+    storageBucket: "ssvtodos.appspot.com",
+    messagingSenderId: "20219089328",
+    appId: "1:20219089328:web:488e8bb0227222b2c54675",
+    measurementId: "G-81GSEP6L1D"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
